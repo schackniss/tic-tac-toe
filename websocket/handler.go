e317a799fced5b96789c88eaec61890e17cf22c5
Johannes Schackniß <johannes@schackniss.de>
2021-06-05 19:04:12 +0200

    Add res as parameter in function handleMessage

--- websocket/handler.go
+++ websocket/handler.go
@@ -1,8 +1,10 @@
 package websocket
 
+import "tic-tac-toe/game"
+
 // handleMessage verarbeitet die Anfrage/Nachricht eines Clients und gibt eine Antwort zurück.
 //
 // Die Nachricht ist der vom Client durchgeführte Spielzug. handleMessage ruft die Spiellogik auf und gibt den neuen Spielstand zurück.
-func handleMessage(msg []byte) []byte {
+func handleMessage(msg []byte, res *game.TicTacToe) []byte {
 	return msg
 }
